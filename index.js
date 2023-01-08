e4c1fedd942ff82185758763aedd45292df5f5a0
Handryc Natan <handrycnatan@pop-os.localdomain>
2023-01-08 17:35:52 -0300

    struct logic verify

--- index.js
+++ index.js
@@ -74,9 +74,9 @@ class Game {
             this.direction
         );
 
-        const isEatItem = this.board[eatItemRow][eatItemCol] != 0;
-        const isItemOnStartSpace = this.board[this.row][this.col] != 0;
-        const isItemOnEndSpace = this.board[endRow][endCol] != 0;
+        const isEatItem = this.board[eatItemRow][eatItemCol] === 1;
+        const isItemOnStartSpace = this.board[this.row][this.col] === 1;
+        const isItemOnEndSpace = this.board[endRow][endCol] === 0;
 
         if(!isItemOnStartSpace){
             throw new Error('Not a item to execute action');
